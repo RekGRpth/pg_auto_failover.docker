c1dfad64ac8cd5f341f0938eb3142bb0d5b08090
RekGRpth <rekgrpth@gmail.com>
2020-04-12 10:46:03 +0500

    up

--- service.sh
+++ service.sh
@@ -10,24 +10,24 @@ docker service rm monitor || echo $?
 docker service create \
     --constraint node.role==manager \
     --hostname tasks.monitor \
-    --mount type=volume,source=monitor,destination=/home \
+    --mount type=volume,source=monitor,destination=/var/lib/postgresql \
     --name monitor \
     --network name=docker \
     --replicas-max-per-node 1 \
-    rekgrpth/pg_auto_failover sh -cx "chown -R postgres:postgres /home && su-exec postgres pg_autoctl -vvv create monitor --nodename tasks.monitor --no-ssl --auth trust && su-exec postgres pg_autoctl -vvv run"
+    rekgrpth/pg_auto_failover sh -cx "su-exec postgres pg_autoctl -vvv create monitor --nodename tasks.monitor --no-ssl --auth trust && su-exec postgres pg_autoctl -vvv run"
 docker service create \
     --constraint node.labels.host==docker-1 \
     --hostname tasks.postgres-1 \
-    --mount type=volume,source=postgres,destination=/home \
+    --mount type=volume,source=postgres,destination=/var/lib/postgresql \
     --name postgres-1 \
     --network name=docker \
     --replicas-max-per-node 1 \
-    rekgrpth/pg_auto_failover sh -cx "chown -R postgres:postgres /home && su-exec postgres pg_autoctl -vvv create postgres --nodename tasks.postgres-1 --no-ssl --auth trust --monitor=postgres://autoctl_node@tasks.monitor:5432/pg_auto_failover && su-exec postgres pg_autoctl -vvv run"
+    rekgrpth/pg_auto_failover sh -cx "su-exec postgres pg_autoctl -vvv create postgres --nodename tasks.postgres-1 --no-ssl --auth trust --monitor=postgres://autoctl_node@tasks.monitor:5432/pg_auto_failover && su-exec postgres pg_autoctl -vvv run"
 docker service create \
     --constraint node.labels.host==docker-2 \
     --hostname tasks.postgres-2 \
-    --mount type=volume,source=postgres,destination=/home \
+    --mount type=volume,source=postgres,destination=/var/lib/postgresql \
     --name postgres-2 \
     --network name=docker \
     --replicas-max-per-node 1 \
-    rekgrpth/pg_auto_failover sh -cx "chown -R postgres:postgres /home && su-exec postgres pg_autoctl -vvv create postgres --nodename tasks.postgres-2 --no-ssl --auth trust --monitor=postgres://autoctl_node@tasks.monitor:5432/pg_auto_failover && su-exec postgres pg_autoctl -vvv run"
+    rekgrpth/pg_auto_failover sh -cx "su-exec postgres pg_autoctl -vvv create postgres --nodename tasks.postgres-2 --no-ssl --auth trust --monitor=postgres://autoctl_node@tasks.monitor:5432/pg_auto_failover && su-exec postgres pg_autoctl -vvv run"
