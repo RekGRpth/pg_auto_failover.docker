2588d1640c95cf99d4143e7390c3c0194fd6ba34
RekGRpth <rekgrpth@gmail.com>
2020-04-13 09:14:29 +0500

    up

--- service.sh
+++ service.sh
@@ -4,8 +4,8 @@ docker pull rekgrpth/pg_auto_failover || exit $?
 docker volume create monitor || echo $?
 docker volume create postgres || echo $?
 docker network create --attachable --driver overlay docker || echo $?
-docker service rm postgres-1 || echo $?
-docker service rm postgres-2 || echo $?
+docker service rm postgres1 || echo $?
+docker service rm postgres2 || echo $?
 docker service rm monitor || echo $?
 docker service create \
     --constraint node.role==manager \
@@ -16,18 +16,18 @@ docker service create \
     --replicas-max-per-node 1 \
     rekgrpth/pg_auto_failover sh -cx "pg_autoctl -vvv create monitor --nodename tasks.monitor --no-ssl --auth trust; pg_autoctl -vvv run"
 docker service create \
-    --constraint node.labels.host==docker-1 \
-    --hostname tasks.postgres-1 \
+    --constraint node.hostname==docker1 \
+    --hostname tasks.postgres1 \
     --mount type=volume,source=postgres,destination=/var/lib/postgresql \
-    --name postgres-1 \
+    --name postgres1 \
     --network name=docker \
     --replicas-max-per-node 1 \
-    rekgrpth/pg_auto_failover sh -cx "pg_autoctl -vvv create postgres --nodename tasks.postgres-1 --no-ssl --auth trust --allow-removing-pgdata --monitor=postgres://autoctl_node@tasks.monitor:5432/pg_auto_failover; pg_autoctl -vvv run"
+    rekgrpth/pg_auto_failover sh -cx "pg_autoctl -vvv create postgres --nodename tasks.postgres1 --no-ssl --auth trust --allow-removing-pgdata --monitor=postgres://autoctl_node@tasks.monitor:5432/pg_auto_failover; pg_autoctl -vvv run"
 docker service create \
-    --constraint node.labels.host==docker-2 \
-    --hostname tasks.postgres-2 \
+    --constraint node.hostname==docker2 \
+    --hostname tasks.postgres2 \
     --mount type=volume,source=postgres,destination=/var/lib/postgresql \
-    --name postgres-2 \
+    --name postgres2 \
     --network name=docker \
     --replicas-max-per-node 1 \
-    rekgrpth/pg_auto_failover sh -cx "pg_autoctl -vvv create postgres --nodename tasks.postgres-2 --no-ssl --auth trust --allow-removing-pgdata --monitor=postgres://autoctl_node@tasks.monitor:5432/pg_auto_failover; pg_autoctl -vvv run"
+    rekgrpth/pg_auto_failover sh -cx "pg_autoctl -vvv create postgres --nodename tasks.postgres2 --no-ssl --auth trust --allow-removing-pgdata --monitor=postgres://autoctl_node@tasks.monitor:5432/pg_auto_failover; pg_autoctl -vvv run"
