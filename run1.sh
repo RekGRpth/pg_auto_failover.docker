58fc7def122cdeec6485ca8b7215d9eb0ae5cfca
RekGRpth <rekgrpth@gmail.com>
2020-06-11 10:55:00 +0500

    up

--- run1.sh
+++ run1.sh
@@ -4,19 +4,20 @@
 #docker push rekgrpth/pg_auto_failover
 #docker pull rekgrpth/pg_auto_failover
 docker network create --attachable --opt com.docker.network.bridge.name=docker docker || echo $?
-docker volume create pg_auto_failover0
-docker stop pg_auto_failover0 || echo $?
-docker rm pg_auto_failover0 || echo $?
+docker volume create pg_auto_failover1
+docker stop pg_auto_failover1 || echo $?
+docker rm pg_auto_failover1 || echo $?
 docker run \
     --detach \
     --env GROUP_ID="$(id -g)" \
     --env LANG=ru_RU.UTF-8 \
+    --env PG_AUTO_FAILOVER_MONITOR=postgres://autoctl_node@pg_auto_failover0:5432/pg_auto_failover?sslmode=prefer \
     --env TZ=Asia/Yekaterinburg \
     --env USER_ID="$(id -u)" \
-    --hostname pg_auto_failover0 \
+    --hostname pg_auto_failover1 \
     --mount type=bind,source=/etc/certs,destination=/etc/certs,readonly \
-    --mount type=volume,source=pg_auto_failover0,destination=/var/lib/postgresql \
-    --name pg_auto_failover0 \
+    --mount type=volume,source=pg_auto_failover1,destination=/var/lib/postgresql \
+    --name pg_auto_failover1 \
     --network name=docker \
     --restart always \
     rekgrpth/pg_auto_failover
