dc9f3afc535db2371fd55d8d55b06f6808133fc5
RekGRpth <rekgrpth@gmail.com>
2020-09-24 11:05:58 +0500

    upo

---
+++ run4.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -ex
+
+#docker build --tag rekgrpth/pg_auto_failover .
+#docker push rekgrpth/pg_auto_failover
+#docker pull rekgrpth/pg_auto_failover
+docker network create --attachable --opt com.docker.network.bridge.name=docker docker || echo $?
+docker volume create pg_auto_failover4
+docker stop pg_auto_failover4 || echo $?
+docker rm pg_auto_failover4 || echo $?
+docker run \
+    --detach \
+    --env GROUP_ID="$(id -g)" \
+    --env LANG=ru_RU.UTF-8 \
+    --env PG_AUTO_FAILOVER_MONITOR=postgres://autoctl_node@pg_auto_failover0/pg_auto_failover?sslmode=prefer \
+    --env TZ=Asia/Yekaterinburg \
+    --env USER_ID="$(id -u)" \
+    --hostname pg_auto_failover4 \
+    --mount type=bind,source=/etc/certs,destination=/etc/certs,readonly \
+    --mount type=volume,source=pg_auto_failover4,destination=/var/lib/postgresql \
+    --name pg_auto_failover4 \
+    --network name=docker \
+    --restart always \
+    rekgrpth/pg_auto_failover
